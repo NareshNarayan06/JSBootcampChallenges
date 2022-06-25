0d6c5c9cd9c8c6f4aab0794d665d9543b8fcc311
Naresh Narayan <nareshnarayan@nareshs-air.myfiosgateway.com>
2022-06-24 22:53:17 -0400

    HomeWork2:strings

--- tests/01_strings/strings.js
+++ tests/01_strings/strings.js
@@ -1,18 +1,41 @@
 /* eslint-disable no-unused-vars */
 
 function doesInclude(str, letter) {
-
+  if ( str.includes(letter)){
+    console.log(`'${str}' does have the letter '${letter}''`)
+  }else{
+    console.log(`'${str}' does not have the letter '${letter}'`)
+  }
 }
 
 function isPalindrome(str) {
+  let reverse = '';
+
+ for ( let i = str.length-1; i >= 0; i-- ) {
+
+     reverse += str.charAt(i);
+ }
+  return str === reverse;
 
 }
 
 function cap(str, letter) {
-
+  let strInCap = str.toUpperCase();
+  let letterInCap = letter.toUpperCase();
+  let indexOfletterInStr = strInCap.indexOf(letterInCap);
+  if(strInCap.includes(letterInCap)){
+  console.log(strInCap.charAt(indexOfletterInStr + 1))
+  }else{
+    console.log(`'${str}' does not have the letter '${letter}'`)
+  }
 }
 
 function firstCharacter(str1, str2) {
+  if(string1.toLowerCase().charAt(0) === string2.toLowerCase().charAt(0)){
+    return true;
+  }else{
+    return false;
+    }
 
 }
 
