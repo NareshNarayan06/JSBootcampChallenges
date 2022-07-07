067430d1d1dc2f40f6b6a2e095f3ba9a3d9b41a1
Naresh Narayan <nareshnarayan@nareshs-air.myfiosgateway.com>
2022-07-06 22:10:39 -0400

    Objects

--- tests/04_objects/objects.js
+++ tests/04_objects/objects.js
@@ -2,21 +2,28 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function deleteProp(obj, prop) {
-
+delete obj[prop];
+return obj;
 }
 
 function numObjectProps(obj) {
+  let count = Object.keys(obj);
+  return count.length
 
 }
 
 function shoppingCart(cart) {
+  let total = 0 ;
+  for (let i = 0 ; i < cart.length ; i++){
+    total += cart[i].amount;
+  }
+  return total ;
 
 }
 
 function compareObjects(obj1, obj2) {
-
+ return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
-
 module.exports = {
   compareObjects, deleteProp, numObjectProps, shoppingCart,
 };
